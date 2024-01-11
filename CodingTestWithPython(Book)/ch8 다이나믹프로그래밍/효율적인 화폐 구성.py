582aa599ec155dba36bb0b736d14307af31289c3
H-Yeji <yeji980603@naver.com>
2024-01-11 23:38:44 +0900

    이것이 코딩테스트다 (예제8-8)

---
+++ CodingTestWithPython(Book)/ch8 다이나믹프로그래밍/효율적인 화폐 구성.py
@@ -0,0 +1,21 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+arr = []
+for i in range(n):
+    arr.append(int(sys.stdin.readline()))
+
+
+d = [10001] * (m + 1)
+d[0] = 0
+for i in range(n):
+    for j in range(arr[i], m + 1):
+        if d[j - arr[i]] != 10001:
+            d[j] = min(d[j], d[j - arr[i]] + 1)
+
+
+if d[m] == 10001:
+    print(-1)
+else:
+    print(d[m])
+
