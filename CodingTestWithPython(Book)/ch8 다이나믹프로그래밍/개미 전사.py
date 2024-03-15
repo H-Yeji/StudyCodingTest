0044dca3a23047d49ec458d4092016c7cd0caf57
H-Yeji <yeji980603@naver.com>
2024-03-15 23:19:58 +0900

    이것이 코딩테스트다 (예제8-6)

--- CodingTestWithPython(Book)/ch8 다이나믹프로그래밍/개미 전사.py
+++ CodingTestWithPython(Book)/ch8 다이나믹프로그래밍/개미 전사.py
@@ -6,7 +6,7 @@ d.append(arr[0])
 d.append(max(d[0], arr[1]))
 # 다이나믹 프로그래밍 진행 (보텀업) 
 for i in range(2, n):
-    d.append(max(d[i - 1], (arr[i - 2] + arr[i])))
+    d.append(max(d[i - 1], (d[i - 2] + arr[i])))
 
 
 print(d[n - 1])
